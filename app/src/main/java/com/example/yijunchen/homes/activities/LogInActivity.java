35dd3203f17b6067bb143f60f7ca6e0238c6bbaf
Yijun Chen <yijun2@g.clemson.edu>
2017-07-06 17:00:24 -0500

    hhh

--- app/src/main/java/com/example/yijunchen/homes/activities/LogInActivity.java
+++ app/src/main/java/com/example/yijunchen/homes/activities/LogInActivity.java
@@ -2,6 +2,8 @@ package com.example.yijunchen.homes.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.example.yijunchen.homes.R;
 
 
@@ -11,5 +13,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_);
+        Log.i("hhhh","hhhhh");
     }
 }
