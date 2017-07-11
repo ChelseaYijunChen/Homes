577e86eacdd9dc0caa377aaaa2055b02922d39ff
Yijun Chen <yijun2@g.clemson.edu>
2017-07-11 14:40:08 -0500

    code review

--- app/src/main/java/com/example/yijunchen/homes/activities/SellerActivity.java
+++ app/src/main/java/com/example/yijunchen/homes/activities/SellerActivity.java
@@ -5,21 +5,60 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.yijunchen.homes.R;
 import com.example.yijunchen.homes.fragments.AddPropertyFragment;
 import com.example.yijunchen.homes.fragments.AllPropertyFragment;
 import com.example.yijunchen.homes.fragments.CategoryTabFragment;
 import com.example.yijunchen.homes.fragments.MapFragment;
 import com.example.yijunchen.homes.fragments.SellerAccountFragment;
+import com.example.yijunchen.homes.models.Property;
+import com.example.yijunchen.homes.models.PropertyList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class SellerActivity extends AppCompatActivity {
+    List<Property> propertyList = PropertyList.getInstance();
     BottomNavigationView bottomNavigationView;
+
+    String GET_JSON_DATA_HTTP_URL = "http://rjtmobile.com/aamir/realestate/realestate_app/getproperty.php";
+    String JSON_PROPERTY_ID = "PropertyId";
+    String JSON_PROPERTY_NAME = "PropertyName";
+    String JSON_PROPERTY_DESC = "PropertyDesc";
+    String JSON_PROPERTY_TYPE = "PropertyType";
+    String JSON_PROPERTY_CATEGORY = "PropertyCategory";
+    String JSON_PROPERTY_ADDRESS1 = "PropertyAddress1";
+    String JSON_PROPERTY_ADDRESS2 = "PropertyAddress2";
+    String JSON_PROPERTY_ZIP = "PropertyZip";
+    String JSON_PROPERTY_LATITUDE = "PropertyLatitute";
+    String JSON_PROPERTY_LONGITUDE = "PropertyLongtitue";
+    String JSON_PROPERTY_THUMB1 = "PropertyThumb1";
+    String JSON_PROPERTY_THUMB2 = "PropertyThumb2";
+    String JSON_PROPERTY_THUMB3 = "PropertyThumb3";
+    String JSON_PROPERTY_COST = "PropertyCost";
+    String JSON_PROPERTY_SIZE = "PropertySize";
+    String JSON_PROPERTY_STATUS = "PropertyStatus";
+    String JSON_PROPERTY_UPDATE = "PropertyUpdate";
+    String JSON_PROPERTY_SELLER_ID = "PropertySellerID";
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (propertyList.size() == 0) {
+            JSON_DATA_WEB_CALL();
+        }
         setContentView(R.layout.activity_seller);
 
         final MapFragment mapFragment = new MapFragment();
@@ -56,5 +95,68 @@ public class SellerActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public void JSON_DATA_WEB_CALL() {
+
+        StringRequest stringRequest = new StringRequest(GET_JSON_DATA_HTTP_URL,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("Property List");
+                            JSON_PARSE_DATA_AFTER_WEBCALL(jsonArray);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void JSON_PARSE_DATA_AFTER_WEBCALL(JSONArray array) {
+
+        for (int i = 0; i < array.length(); i++) {
+
+            Property property = new Property();
+
+            try {
+                JSONObject json = array.getJSONObject(i);
+                property.setAddress1(json.getString(JSON_PROPERTY_ADDRESS1));
+                property.setAddress2(json.getString(JSON_PROPERTY_ADDRESS2));
+                property.setCategory(json.getInt(JSON_PROPERTY_CATEGORY));
+                property.setCost(json.getDouble(JSON_PROPERTY_COST));
+                property.setDescription(json.getString(JSON_PROPERTY_DESC));
+                property.setId(json.getInt(JSON_PROPERTY_ID));
+                property.setSellerId(json.getInt(JSON_PROPERTY_SELLER_ID));
+                property.setImgThumb1(json.getString(JSON_PROPERTY_THUMB1));
+                property.setImgThumb2(json.getString(JSON_PROPERTY_THUMB2));
+                property.setImgThumb3(json.getString(JSON_PROPERTY_THUMB3));
+                property.setLatitude(Float.parseFloat(json.getString(JSON_PROPERTY_LATITUDE)));
+                property.setLongitude(Float.parseFloat(json.getString(JSON_PROPERTY_LONGITUDE)));
+                property.setName(json.getString(JSON_PROPERTY_NAME));
+                property.setSize(json.getInt(JSON_PROPERTY_SIZE));
+                property.setStatus(json.getString(JSON_PROPERTY_STATUS));
+                property.setZipCode(json.getInt(JSON_PROPERTY_ZIP));
+                property.setType(json.getString(JSON_PROPERTY_TYPE));
+                property.setUpdate(json.getString(JSON_PROPERTY_UPDATE));
+
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+            propertyList.add(property);
+            Log.v("main property", property.toString());
+        }
     }
 }
